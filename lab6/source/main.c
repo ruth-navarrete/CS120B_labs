76591fab126ad72ec0c4b37f2e9f3c7e9d9929e3
Ruth Navarrete <rnava021@ucr.edu>
2020-01-28 16:21:48 -0800

    In progress lab 6 part 1

--- lab6/source/main.c
+++ lab6/source/main.c
@@ -1,8 +1,9 @@
 /*	Author: rnava021
  *  Partner(s) Name: Dylan McDowell
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 24
+ *	Assignment: Lab 6  Exercise 1
  *	Exercise Description: [optional - include for your own benefit]
+ *  Create a synchSM to blink three LEDs connected to PB0, PB1, and PB2 in sequence, 1 second each.
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -12,12 +13,57 @@
 #include "simAVRHeader.h"
 #endif
 
+enum States {start, led1, led2, led3} state;
+
+volatile unsigned char TimerFlag = 0;
+void TimerISR() { TimerFlag = 1; }
+void Tick() {
+    switch (state) { /* transitions */
+    case start:
+        state = led1;
+        break;
+    case led1:
+        state = led2;
+        break;
+    case led2;
+        state = led3;
+        break;
+    case led3:
+        state = led1;
+        break;
+    default:
+        state = start;
+        break;
+    } /* transitions */
+    
+    switch (state) { /* actions */
+    case start:
+        break;
+    case led1:
+        PORTB = 0x01;
+        break;
+    case led2;
+        PORTB = 0x02;
+        break;
+    case led3:
+        PORTB = 0x04;
+        break;
+    default:
+        break;
+    }
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRB = 0xFF; PORTB = 0x00; /* Configure PORTB as output, initialized to all 0s*/
+    state = start;
+    TimerSet(1000);
+    TimerOn();
 
-    /* Insert your solution below */
     while (1) {
-
+        Tick();
+        while (!TimerFlag) {}
+        TimerFlag = 0;
     }
     return 1;
 }

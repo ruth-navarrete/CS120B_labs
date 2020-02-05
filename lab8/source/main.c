6c5a98307a7008c5f0719087d8410b1a90da0ef5
Dylan McDowell <dmcdo006@ucr.edu>
2020-02-04 16:40:59 -0800

    Implemented lab8 part2

--- lab8/source/main.c
+++ lab8/source/main.c
@@ -12,12 +12,25 @@
 #include "simAVRHeader.h"
 #endif
 
+void ADC_init() {
+    ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
+
 int main(void) {
-    /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
+    DDRA = 0x00; PORTA = 0xFF; //Configure port A's 8 pins as inputs
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRD = 0xFF; PORTD = 0x00; //Configure port B and D's pins to outputs
+                               //Initialize both to zero
+    ADC_init();
+    unsigned short l_short;
+    unsigned char lu_char;
+    unsigned char ll_char;
     while (1) {
-
+        l_short = ADC;
+        ll_char = (char)l_short;
+        lu_char = (char)(l_short >> 8);
+        PORTB = ll_char;
+        PORTD = lu_char;
     }
     return 1;
 }

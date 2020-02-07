95cd5a56e3a9bd6c46e9047cfd91e449ae60b1b2
Dylan McDowell <dmcdo006@ucr.edu>
2020-02-06 16:53:08 -0800

    Lab 8 part 2 in progress

--- lab9/source/main.c
+++ lab9/source/main.c
@@ -1,8 +1,16 @@
 /*	Author: rnava021, dmcdo006
  *  Partner(s) Name: Ruth Navarrete, Dylan McDowell
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 24
+ *	Assignment: Lab #9  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
+ *	Using the ATmega1284’s PWM functionality, design a system where the notes: 
+ *	C4, D, E, F, G, A, B, and C5,  from the table at the top of the lab, can 
+ *	be generated on the speaker by scaling up or down the eight note scale. 
+ *	Three buttons are used to control the system. One button toggles sound on/off. 
+ *	The other two buttons scale up, or down, the eight note scale. Criteria:
+ *		The system should scale up/down one note per button press.
+ *		When scaling down, the system should not scale below a C.
+ *		When scaling up, the system should not scale above a C.
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -12,12 +20,58 @@
 #include "simAVRHeader.h"
 #endif
 
+void set_PWM(double frequency) {
+    static double current_frequency;
+
+    if(frequency != current_frequency) {
+        if(!frequency) { TCCR3B &= 0x08; }
+        else { TCCR3B |= 0x03; }
+
+        if (frequency < 0.954) { OCR3A = 0xFFFF; }
+
+        else if(frequency > 31250) { OCR3A = 0x0000; }
+
+        else {OCR3A = (short) ( 8000000 / (128 * frequency))-1;}
+
+        TCNT3 = 0;
+        current_frequency = frequency;
+    }
+}
+
+void PWM_on() {
+    TCCR3A = (1 << COM3A0);
+    TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+    set_PWM(0);
+}
+
+void PWM_off() {
+    TCCR3A = 0x00;
+    TCCR3B = 0x00;
+}
+
+enum States {start, init,  } state;
+
+volatile unsigned char TimerFlag = 0;
+void TimerISR() { TimerFlag = 1; }
+double freq;
+
+//void Tick() {};
+
 int main(void) {
-    /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
+    DDRA = 0x00; PINA = 0xFF; // Initialize port A's 8 pins to input
+    DDRB = 0xFF; PORTB = 0x00; // Initialize port B's 8 pins to output
+                               // Set port B's pins to 0
+    PWM_on();
+    TimerSet(100);
+    TimerOn();
+    freq = 0;
     while (1) {
-
+         //Tick();
+         set_PWM(261.63);
+         while(!TimerFlag){}
+         TimerFlag = 0;
     }
     return 1;
 }
+
+
